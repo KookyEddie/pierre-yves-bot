959d0401cda5b3062bc1ee89a0d4dd5fd21be9c2
Xavier Mathieu <xavier2003@hotmail.fr>
2022-02-03 21:35:31 -0500

    npm install lol-discord pour l'api de match history

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 require("dotenv").config()
 
-const generateImage = require("./generateImage")
-const Discord = require("discord.js")
+import generateImage from "./generateImage";
+import { Client } from "discord.js";
+import { SetLanguage, SetLocale } from 'lol-discord';
 
-const client = new Discord.Client({
+const client = new Client({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES",
@@ -13,11 +14,29 @@ const client = new Discord.Client({
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
+    SetLanguage("en");
+    SetLocale("na1");
 })
 
 client.on("messageCreate", (message) => {
     if (message.content == "!ranked"){
         message.reply("Tu es Iron IV!")
+    }
+})
+
+client.on("messageCreate", (message) => {
+    let validateCommand = message.content.slice(0, 7)
+    if (validateCommand == "!history" && message.content.length > 7){
+        let playerName = message.content.slice(8);
+        message.reply("Attend un peu").then(message_searching => {
+            lol.Search(playerName).then(embed_message => {
+                message_searching.delete();
+                message.reply({embed: embed_message});
+            }).catch(err => {
+                message_searching.delete();
+                message.reply(err);
+            });
+          });
     }
 })
 
