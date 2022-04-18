484c5b15fd4968de524648fc0ea15cc80285d431
Hugo Larochelle <hugolarochelle@Hugos-MacBook-Pro.local>
2022-04-17 20:59:27 -0400

    add music options

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-//const generateImage = require("./generateImage")
+const { Routes } = require("discord-api-types/v9")
 
+const fs = require("fs")
+
+const { Player } = require("discord-player")
+const { REST } = require("@discordjs/rest")
+
+const TOKEN = process.env.TOKEN
+
+const LOAD_SLASH = process.argv[2] == "load"
+
+const CLIENT_ID = "938641957150924830"
+const GUILD_ID = "623642120615493632"
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
         "GUILD_MESSAGES",
-        "GUILD_MEMBERS"
+        "GUILD_MEMBERS",
+        "GUILD_VOICE_STATES"
     ]
 })
 
@@ -21,6 +33,54 @@ let bot = {
 
 client.commands = new Discord.Collection()
 client.events = new Discord.Collection()
+client.slashcommands = new Discord.Collection()
+
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+
+let commands = []
+
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+for (const file of slashFiles){
+    const slashcmd = require(`./slash/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+    .then(() => {
+        console.log("Successfully loaded")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err){
+            console.log(err)
+            process.exit(1)
+        }
+    })
+}
+else {
+    client.on("interactionCreate", (interaction) => {
+        async function handleCommand() {
+            if (!interaction.isCommand()) return
+
+            const slashcmd = client.slashcommands.get(interaction.commandName)
+            if (!slashcmd) interaction.reply("Not a valid slash command")
+
+            await interaction.deferReply()
+            await slashcmd.run({ client, interaction })
+        }
+        handleCommand()
+    })
+    client.login(TOKEN)
+}
 
 client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload)
 client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
@@ -29,25 +89,5 @@ client.loadEvents(bot, false)
 client.loadCommands(bot, false)
 
 module.exports = bot
-
-/* client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
-
-client.on("messageCreate", (message) => {
-    if (message.content == "!ranked"){
-        message.reply("Tu es Iron IV!")
-    }
-})
-
-const welcomeChannelId = "733165385527984211"
-
-client.on("guildMemberAdd", async (member) => {
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChannelId).send({
-        content:`<@${member.id}> Bienvenue sur le serveur!`,
-        files: [img]
-    })
-}) */
 
 client.login(process.env.TOKEN)
